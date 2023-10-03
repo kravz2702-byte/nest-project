013e91fbb8a566203d42561f0b69be6557fdf407
kravz2702-byte <kravz2702@gmail.com>
2023-10-03 22:55:54 +0300

    new controller and templates

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import { NestFactory } from '@nestjs/core';
+import * as hbs from 'hbs';
+import * as hbsUtils from 'hbs-utils';
+import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  hbs.registerPartials(join(__dirname, '..', 'views/layouts'));
+  hbsUtils(hbs).registerWatchedPartials(join(__dirname, '..', 'views/layouts'));
+  app.setViewEngine('hbs');
+  await app.listen(3000);
+}
+bootstrap();

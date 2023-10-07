10300bce11f35c387a48d88c70a3bc65af72f8cd
kravz2702-byte <kravz2702@gmail.com>
2023-10-08 00:16:38 +0300

    add module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { ProductsController } from './products.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProductsService } from './models/products.service';
+import { Product } from './models/product.entity';
+import { AdminModule } from './admin/admin.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '',
+      database: 'online_store',
+      entities: ['dist/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    TypeOrmModule.forFeature([Product]),
+    AdminModule,
+  ],
   controllers: [AppController, ProductsController],
+  providers: [ProductsService],
 })
 export class AppModule {}
